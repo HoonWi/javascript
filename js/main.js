3becac30bdb1a58b210c1bec241f003698eb772b
HoonWi <wee0702@gmail.com>
2023-01-11 02:04:32 +0900

    Add function add item

--- js/main.js
+++ js/main.js
@@ -16,4 +16,48 @@ $(function () {
         $loadMoreBtn.click(addItem); //클릭하면 아이템 추가
     }//initGallery
 
+    function addItem(){
+        var slicedData;
+        var elements = [];
+
+        slicedData = $allData.slice($added, $added+$addItemCount); //데이터 3개씩 호출
+        console.log(slicedData);
+
+        // $.each('배열', function(idx, item){}); // JSON 배열값마다 할 일
+        $.each(slicedData, function(idx, item){
+            var itemHTML =
+            '<li class="gallery-item">' + 
+                '<a href="#">' +
+                    '<figure>' +
+                        '<img src="' + item.images.thumb + '"alt="' + item.title + '">' +
+                        '<figcaption>' + 
+                        item.title +
+                        '</figcaption>' +
+                    '</figure>' +
+                '</a>' +
+            '</li>' ;
+
+
+            elements.push($(itemHTML).get(0));
+        });
+        
+        console.log(elements);
+        $container.append(elements);
+        
+        $added += slicedData.length;
+
+        if($added < $allData.length){
+            $loadMoreBtn.show()
+        } else {
+            $loadMoreBtn.hide()
+        }
+
+        $container.imagesLoaded(function() {
+            // images have loaded
+            $container.masonry('appended', elements )
+        });
+        
+          
+
+    }//addItem
 }); //ready function
